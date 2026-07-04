eb4622e58da5b15c60a3bca1c674481cd1873fd4
William Step <william.step.0205@gmail.com>
2026-07-03 18:03:53 -0700

    Revert "Update webhost to match replit preferences"

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://0.0.0.0:5000"); // replit
-// builder.WebHost.UseUrls("http://localhost:5000/"); // local host
+// builder.WebHost.UseUrls("http://0.0.0.0:5000"); // replit
+builder.WebHost.UseUrls("http://localhost:5000/"); // local host
 var app = builder.Build();
 
 app.UseDefaultFiles();
