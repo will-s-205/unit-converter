d2a3d509b6b498470d9fda95d14d501b4a6192ec
William Step <william.step.0205@gmail.com>
2026-07-04 14:30:00 -0700

    add docker config file

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://0.0.0.0:5000"); // replit // comment this line if want to run locally
+// builder.WebHost.UseUrls("http://0.0.0.0:5000"); // replit // comment this line if you want to run locally
 var app = builder.Build();
 
 app.UseDefaultFiles();
